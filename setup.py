8a18f8e07b6f7d2a9e322492847871ac34db14b3
HelpRam <rammey115@gmail.com>
2024-06-03 21:55:53 +0545

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="signlanguage",
+    version="0.0.0",
+    author="HelpRam",
+    author_email="rammey115@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
